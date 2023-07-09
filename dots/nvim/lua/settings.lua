00f345b429e8b85124cddc573e0d36ad62332f6f
Ben Marr <benmarr2@gmail.com>
2023-07-09 18:50:26 +0000

    Add files via upload
    blah

---
+++ dots/nvim/lua/settings.lua
@@ -0,0 +1,13 @@
+vim.cmd('set rnu nu')
+vim.cmd('set noswapfile')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
+vim.cmd('set expandtab')
+
+
+vim.g.material_style = "deep ocean"
+vim.cmd('colorscheme material')
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
